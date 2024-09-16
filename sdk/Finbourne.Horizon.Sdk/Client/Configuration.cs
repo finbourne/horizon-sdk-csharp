1fa0aaf553ef0257fe5df3a43742a5d9c8f7b0d0
Concourse <concourse@finbourne.com>
2024-09-16 14:18:07 +0000

    horizon csharp updates for v2.1.140
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/Finbourne.Horizon.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Horizon.Sdk/Client/Configuration.cs
@@ -594,7 +594,7 @@ namespace Finbourne.Horizon.Sdk.Client
             string report = "C# SDK (Finbourne.Horizon.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.1.751\n";
+            report += "    Version of the API: 0.1.752\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
